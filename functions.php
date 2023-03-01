e149b1b00223f49bd1b9549cf7da5afe963a2402
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-03-02 00:30:10 +0100

    Modif CSS + functions
    --> size du body 1440px;
    --> suppression de la fonction register_menu.

--- functions.php
+++ functions.php
@@ -7,11 +7,3 @@ function theme_enqueue_style()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('child-style',get_stylesheet_uri(), array('parent-style'));
 }
-
-function register_planty_menu() {
-    register_nav_menu('Primary menu',__( 'Planty Menu' ));
-   }
-
- add_action( 'init', 'register_planty_menu' );
-  
-?>  
