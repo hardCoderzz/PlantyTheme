f32897a37b5cd8ecd51ff7f7c536f5dc0746d74e
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-03-15 23:04:05 +0100

    Syntaxe
    ajout d'un espace dans wp_enqueue_style()

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 function theme_enqueue_style()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-style',get_stylesheet_uri(), array('parent-style'));
+    wp_enqueue_style('child-style', get_stylesheet_uri(), array('parent-style'));
 }
 
 add_action('wp_enqueue_scripts', 'theme_enqueue_style');
